b614e0207eb1c0dc910719061b7be86d9235e656
Tim Cameron Ryan <id@timryan.org>
2012-03-14 00:26:39 -0400

    test

--- app.py
+++ app.py
@@ -22,19 +22,6 @@ def fwolin_auth():
 
 @app.route('/login/', methods=['GET'])
 def login():
-	assertion = request.cookies.get('browserid')
-	if assertion:
-		r = requests.post("https://browserid.org/verify", data={"assertion": assertion, "audience": "fwol.in"})
-		ret = json.loads(r.text)
-		if ret['status'] == 'okay':
-			domain = re.sub(r'^[^@]+', '', ret['email'])
-			if domain in ['@students.olin.edu', '@alumni.olin.edu', '@olin.edu']:
-				session['assertion'] = assertion
-				session['email'] = ret['email']
-				return redirect(url_for('index'))
-		else:
-			request.set_cookie('browserid', None)
-
 	return render_template('login.html')
 
 # Launch
