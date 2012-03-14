983acb3a0797edc9cf8055c082f5c096b2e32716
Tim Cameron Ryan <id@timryan.org>
2012-03-14 00:36:29 -0400

    Fix?

--- app.py
+++ app.py
@@ -22,17 +22,19 @@ def fwolin_auth():
 
 @app.route('/login/', methods=['GET'])
 def login():
-	assertion = request.cookies.get('browserid')
-	if assertion:
-		r = requests.post("https://browserid.org/verify", data={"assertion": assertion, "audience": "fwol.in"})
-		ret = json.loads(r.text)
-		if ret['status'] == 'okay':
-			domain = re.sub(r'^[^@]+', '', ret['email'])
-			if domain in ['@students.olin.edu', '@alumni.olin.edu', '@olin.edu']:
-				session['assertion'] = assertion
-				session['email'] = ret['email']
-				return redirect(url_for('index'))
-
+	if not session.has_key('assertion') or session['assertion'] != browserid:
+		assertion = request.cookies.get('browserid')
+		if assertion:
+			r = requests.post("https://browserid.org/verify", data={"assertion": assertion, "audience": "fwol.in"})
+			ret = json.loads(r.text)
+			if ret['status'] == 'okay':
+				domain = re.sub(r'^[^@]+', '', ret['email'])
+				if domain in ['@students.olin.edu', '@alumni.olin.edu', '@olin.edu']:
+					session['assertion'] = assertion
+					session['email'] = ret['email']
+					return redirect(url_for('index'))
+					
+	# Fall through.
 	return render_template('login.html')
 
 # Launch
